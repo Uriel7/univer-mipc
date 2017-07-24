bf89481e60e074b0597f902031e95c7a4cc01722
Uriel <j.urielsz@gmail.com>
2017-07-24 15:46:41 -0500

    To database with mysql

--- config/database.php
+++ config/database.php
@@ -39,37 +39,21 @@ return [
             'prefix' => '',
         ],
 
-        // 'mysql' => [
-        //     // 'driver' => 'mysql',
-        //     // 'host' => env('DB_HOST', '127.0.0.1'),
-        //     // 'port' => env('DB_PORT', '3306'),
-        //     // 'database' => env('DB_DATABASE', 'forge'),
-        //     // 'username' => env('DB_USERNAME', 'forge'),
-        //     // 'password' => env('DB_PASSWORD', ''),
-        //     // 'unix_socket' => env('DB_SOCKET', ''),
-        //     // 'charset' => 'utf8mb4',
-        //     // 'collation' => 'utf8mb4_unicode_ci',
-        //     // 'prefix' => '',
-        //     // 'strict' => true,
-        //     // 'engine' => null,
-
-        // ],
-
         'mysql' => [
-          'read' => [
-            'host' => 'localhost',
-          ],
-          'write' => [
-            'host' => 'localhost'
-          ],
-          'driver'    => 'mysql',
-          'database'  => 'univer_mipc',
-          'username'  => 'uriel',
-          'password'  => 'juriel20',
-          'charset'   => 'utf8',
-          'collation' => 'utf8_unicode_ci',
-          'prefix'    => '',
-          ],
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
